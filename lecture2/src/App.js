4fa0e959695cf3ba471b8af40d80c61921fd7ca4
jaeuk lim <applewodnr@naver.com>
2022-12-11 17:50:12 +0900

    feat: 올림픽 사진 모달에 지연로딩 추가

--- lecture2/src/App.js
+++ lecture2/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import styled from 'styled-components';
 import Header from './components/Header';
 import InfoTable from './components/InfoTable';
 import SurveyChart from './components/SurveyChart';
 import Footer from './components/Footer';
-import ImageModal from './components/ImageModal';
+
+const LazyImageModal = lazy(() => import('./components/ImageModal'));
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -22,13 +23,16 @@ function App() {
       </ButtonModal>
       <SurveyChart />
       <Footer />
-      {showModal ? (
-        <ImageModal
-          closeModal={() => {
-            setShowModal(false);
-          }}
-        />
-      ) : null}
+
+      <Suspense fallback={null}>
+        {showModal ? (
+          <LazyImageModal
+            closeModal={() => {
+              setShowModal(false);
+            }}
+          />
+        ) : null}
+      </Suspense>
     </div>
   );
 }

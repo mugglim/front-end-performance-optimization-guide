f9d956c4167af1f018584aa548e75806b820458c
jaeuk lim <applewodnr@naver.com>
2022-12-11 17:29:52 +0900

    chore: local prettier 포맷팅 적용

--- lecture2/src/components/Footer.js
+++ lecture2/src/components/Footer.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 function Footer() {
-	return (
-		<FooterWrapper>
-			©Performance Optimization Lecture for React
-		</FooterWrapper>
-	)
+  return (
+    <FooterWrapper>©Performance Optimization Lecture for React</FooterWrapper>
+  );
 }
 
 const FooterWrapper = styled.div`
   text-align: center;
   padding: 32px 0;
-`
+`;
 
-export default Footer
+export default Footer;

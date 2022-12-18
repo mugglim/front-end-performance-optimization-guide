d63426a0f2aa03e961831c9ea11a8d50834dbf4d
jaeuk lim <applewodnr@naver.com>
2022-12-18 17:48:08 +0900

    feat: FOIT 방식과 폰트 로딩 시점을 디텍션하여 fade-out 효과 적용

--- lecture-3/src/components/BannerVideo.js
+++ lecture-3/src/components/BannerVideo.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
+
 import video from '../assets/banner-video.mp4';
 
+const font = new FontFaceObserver('BMYEONSUNG');
+
 function BannerVideo() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  useEffect(() => {
+    font.load(null, 20_00).then(() => {
+      setIsFontLoaded(true);
+    });
+  }, []);
+
+  console.log(isFontLoaded);
+
   return (
     <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
       <div className="absolute h-screen w-full left-1/2">
@@ -13,7 +27,13 @@ function BannerVideo() {
           muted
         />
       </div>
-      <div className="w-full h-full flex justify-center items-center">
+      <div
+        className="w-full h-full flex justify-center items-center"
+        style={{
+          transition: 'opacity 0.4s ease-in-out',
+          opacity: isFontLoaded ? 1 : 0,
+        }}
+      >
         <div className="text-white text-center">
           <div className="text-6xl leading-none font-semibold">KEEP</div>
           <div className="text-6xl leading-none font-semibold">CALM</div>

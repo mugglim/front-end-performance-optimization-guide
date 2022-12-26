b551861ed039e5582766d622bdeaa87eaa659400
jaeuk lim <applewodnr@naver.com>
2022-12-26 16:40:38 +0900

    feat: useSelector에 Eqaulity Function 사용

--- lecture-4/src/containers/ImageModalContainer.js
+++ lecture-4/src/containers/ImageModalContainer.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const modalVisible = useSelector(state => state.imageModal.modalVisible);
-  const bgColor = useSelector(state => state.imageModal.bgColor);
-  const src = useSelector(state => state.imageModal.src);
-  const alt = useSelector(state => state.imageModal.alt);
+  const { modalVisible, bgColor, src, alt } = useSelector(
+    state => ({
+      modalVisible: state.imageModal.modalVisible,
+      bgColor: state.imageModal.bgColor,
+      src: state.imageModal.src,
+      alt: state.imageModal.alt,
+    }),
+    shallowEqual
+  );
 
   return <ImageModal modalVisible={modalVisible} bgColor={bgColor} src={src} alt={alt} />;
 }
